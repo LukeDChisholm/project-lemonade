ba4cc1bc53d3ecd80a5588b2d4f7c243a00f30c0
Luke Chisholm <lukechisholm@tamu.edu>
2019-10-14 21:15:24 -0500

    new app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/', to: 'stores#index'
+  get 'stores/index'
+  get 'stores/show'
+  get 'stores/new'
+  get 'stores/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
