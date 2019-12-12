45a0412811e0139656a0a3a94519ee3c1d388193
Luke Chisholm <lukechisholm@tamu.edu>
2019-12-12 17:42:05 -0600

    database sqlite local

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_10_15_021303) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "stores", force: :cascade do |t|
     t.string "product"
     t.datetime "created_at", null: false
